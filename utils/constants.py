aaa0413aac23251f95fa5627be6ea7b27b45cb3d
Paul H Choi <paulhchoi+github@pm.me>
2024-12-27 02:17:17 -0500

    delete old files

--- utils/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-SCRIPT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-TEMPLATE_DIR = os.path.join(SCRIPT_DIR, "templates")
-
-OUTPUT_BASE_DIR = os.path.join(SCRIPT_DIR, "data")
